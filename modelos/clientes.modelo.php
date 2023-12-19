97dd3878f04b248bdd4513f4e6fd34ed3206d5e6
Datasyslatam <137103078+Datasyslatam@users.noreply.github.com>
2023-12-19 17:14:26 -0500

    Clientes ciudad add
    Adicionar ciudad como nuevo campo

--- modelos/clientes.modelo.php
+++ modelos/clientes.modelo.php
@@ -5,12 +5,13 @@ class ModeloClientes{
 	CREAR CLIENTE
 	=============================================*/
 	public static function mdlIngresarCliente($tabla, $datos){
-		$stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(nombre, documento, email, telefono, direccion, fecha_nacimiento) VALUES (:nombre, :documento, :email, :telefono, :direccion, :fecha_nacimiento)");
+		$stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(nombre, documento, email, telefono, direccion, ciudad, fecha_nacimiento) VALUES (:nombre, :documento, :email, :telefono, :direccion, :ciudad, :fecha_nacimiento)");
 		$stmt->bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
 		$stmt->bindParam(":documento", $datos["documento"], PDO::PARAM_INT);
 		$stmt->bindParam(":email", $datos["email"], PDO::PARAM_STR);
 		$stmt->bindParam(":telefono", $datos["telefono"], PDO::PARAM_STR);
 		$stmt->bindParam(":direccion", $datos["direccion"], PDO::PARAM_STR);
+		$stmt->bindParam(":ciudad", $datos["ciudad"], PDO::PARAM_STR);
 		$stmt->bindParam(":fecha_nacimiento", $datos["fecha_nacimiento"], PDO::PARAM_STR);
 		if($stmt->execute()){
 			$id = Conexion::conectar()->lastInsertId();
