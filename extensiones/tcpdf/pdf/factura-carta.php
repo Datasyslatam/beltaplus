41e7caa1a7bf2e5019a6f364a04b7c63c2d606b5
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2023-12-21 17:52:40 -0500

    cambio orden casillas

--- extensiones/tcpdf/pdf/factura-carta.php
+++ extensiones/tcpdf/pdf/factura-carta.php
@@ -200,9 +200,9 @@ EOF;
 		<tr>
 		
 		<td style="border: 1px solid #666; background-color:white; width: 152px; text-align:center">Producto</td>
-		<td style="border: 1px solid #666; background-color:white; width:  70px; text-align:center">Cantidad</td>
 		<td style="border: 1px solid #666; background-color:white; width:  50px; text-align:center">Talla</td>
 		<td style="border: 1px solid #666; background-color:white; width:  56px; text-align:center">Color</td>
+		<td style="border: 1px solid #666; background-color:white; width:  70px; text-align:center">Cantidad</td>
 		<td style="border: 1px solid #666; background-color:white; width:  56px; text-align:center">Valor Unit.</td>
 		<td style="border: 1px solid #666; background-color:white; width:  56px; text-align:center">Valor por mayor.</td>
 		<td style="border: 1px solid #666; background-color:white; width: 100px; text-align:center">Valor Total</td>
@@ -269,14 +269,14 @@ EOF;
 					$item[descripcion]
 				</td>
 
-				<td style="border: 1px solid #666; color:#333; background-color:white; width: 70px; text-align:center">
-					$cantidad
-				</td>
 				<td style="border: 1px solid #666; color:#333; background-color:white; width: 50px; text-align:center">
 					$talla
 				</td>
 				<td style="border: 1px solid #666; color:#333; background-color:white; width: 56px; text-align:center">
 					$color
+				</td>
+				<td style="border: 1px solid #666; color:#333; background-color:white; width: 70px; text-align:center">
+					$cantidad
 				</td>
 
 				<td style="border: 1px solid #666; color:#333; background-color:white; width: 56px; text-align:center"> 
