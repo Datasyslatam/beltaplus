6795f9da1292cce34034f769916afcd612a5ad01
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2023-12-22 07:56:02 -0500

    test align

--- extensiones/tcpdf/pdf/factura-carta.php
+++ extensiones/tcpdf/pdf/factura-carta.php
@@ -89,7 +89,10 @@ class imprimirFactura
 				</td>
 				<td style="width: 110px; text-align: right; color: red; font-weight: bold; $tdStyle;">
 					<div style="text-align: right; line-height: 15px; margin-top: 50px;">
-						<br><br>PEDIDO No.<br>$valorVenta
+						<br>
+						<br>
+						<br>
+						PEDIDO No.<br>$valorVenta
 					</div>
 				</td>
 			</tr>
