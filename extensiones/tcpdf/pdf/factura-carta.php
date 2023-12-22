e816a87e7752401c0f843c595fb6f9c2564c98a1
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2023-12-22 09:22:12 -0500

    Update factura-carta.php

--- extensiones/tcpdf/pdf/factura-carta.php
+++ extensiones/tcpdf/pdf/factura-carta.php
@@ -70,11 +70,8 @@ class imprimirFactura
 		$bloque1 = <<<EOF
 			<table border="0" style="$tableStyle">
 				<tr>
-					<td style="width: 150px; $tdStyle">
-						<div>
-							</br>
-							<img src="images/logoBeltaFull.png">
-						</div>
+					<td style="width: 150px; padding-top: 50px; $tdStyle">
+						<img src="images/logoBeltaFull.png">
 					</td>
 					<td style="width: 140px; $tdStyle">
 						<div style="font-size: 8.5px; text-align: right; line-height: 14px;">
