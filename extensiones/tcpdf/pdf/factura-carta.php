17d3b744e87dd0fae55fd630394e5a31c5e4862b
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2023-12-22 07:24:39 -0500

    test align

--- extensiones/tcpdf/pdf/factura-carta.php
+++ extensiones/tcpdf/pdf/factura-carta.php
@@ -94,7 +94,7 @@ $bloque1 = <<<EOF
             </td>
 
             <td style="width: 110px; text-align: right; color: red; font-weight: bold; $tdStyle;">
-                <div style="text-align: right; line-height: 15px; padding-top: 50px;">
+                <div style="text-align: right; line-height: 15px; margin-top: 50px;">
                     <br><br>PEDIDO No.<br>$valorVenta
                 </div>
             </td>
