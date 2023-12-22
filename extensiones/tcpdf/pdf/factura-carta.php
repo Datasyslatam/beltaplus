f691c80b5964bd498c815a18dafe28ca36ab25bc
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2023-12-22 09:33:41 -0500

    Update factura-carta.php

--- extensiones/tcpdf/pdf/factura-carta.php
+++ extensiones/tcpdf/pdf/factura-carta.php
@@ -71,7 +71,7 @@ class imprimirFactura
 			<table border="0" style="$tableStyle">
 				<tr>
 					<td style="width: 150px; $tdStyle">
-						{$pdf->Image('images/logoBeltaFull.png', $pdf->GetX(), $pdf->GetY() + 10, 36, 36)}
+						{$pdf->Image('images/logoBeltaFull.png', $pdf->GetX(), $pdf->GetY() + 10, 34)}
 					</td>
 					<td style="width: 140px; $tdStyle">
 						<div style="font-size: 8.5px; text-align: right; line-height: 14px;">
