c85657db68bf23edff4a469979afc35fabb77390
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2024-01-06 19:46:43 -0500

    reset

--- vistas/js/ventas.js
+++ vistas/js/ventas.js
@@ -115,8 +115,8 @@ $(".tablaVentas tbody").on("click", "button.agregarProducto", function () {
                 method: "GET",
                 data: {
                     action: "mostrarColor",
-                    itemColores: idProducto,
-                    valorColores: respuesta["id_color"],
+                    itemTalla: idProducto,
+                    valorTalla: respuesta["id_color"],
                 },
                 dataType: "json",
                 success: function (color) {
