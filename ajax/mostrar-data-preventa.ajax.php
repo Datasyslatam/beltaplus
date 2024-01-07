57bdf27ffb3395bc9b4c987a6d58dd8ecd67cd35
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2024-01-06 19:21:44 -0500

    cambio tipo de peticion

--- ajax/mostrar-data-preventa.ajax.php
+++ ajax/mostrar-data-preventa.ajax.php
@@ -4,26 +4,24 @@ require_once "../modelos/tallas.modelo.php";
 require_once "../controladores/colores.controlador.php";
 require_once "../modelos/colores.modelo.php";
 
-if (isset($_POST["action"]) && !empty($_POST["action"])) {
-    $action = $_POST["action"];
+if (isset($_GET["action"]) && !empty($_GET["action"])) {
+    $action = $_GET["action"];
     switch ($action) {
         case "mostrarTalla":
-
-            $item = isset($_POST["itemTalla"]) ? $_POST["itemTalla"] : "";
-            $valor = isset($_POST["valorTalla"]) ? $_POST["valorTalla"] : "";
+            $item = isset($_GET["itemTalla"]) ? $_GET["itemTalla"] : "";
+            $valor = isset($_GET["valorTalla"]) ? $_GET["valorTalla"] : "";
             $respuesta = ControladorTallas::ctrMostrarTallas($item, $valor);
             echo json_encode($respuesta);
             break;
 
         case "mostrarColor":
-
-            $itemColores = isset($_POST["itemColores"]) ? $_POST["itemColores"] : "";
-            $valorColores = isset($_POST["valorColores"]) ? $_POST["valorColores"] : "";
+            $itemColores = isset($_GET["itemColores"]) ? $_GET["itemColores"] : "";
+            $valorColores = isset($_GET["valorColores"]) ? $_GET["valorColores"] : "";
             $respuestaColores = ControladorColores::ctrMostrarColores($itemColores, $valorColores);
             echo json_encode($respuestaColores);
             break;
-
     }
 }
+
 
 ?>
