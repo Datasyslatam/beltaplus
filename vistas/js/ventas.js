5d4638e0725f55faa9295e62aee7f1be29148bfc
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2024-01-08 09:37:31 -0500

    uso modulo

--- vistas/js/ventas.js
+++ vistas/js/ventas.js
@@ -535,29 +535,7 @@ $(".formularioVenta").on("change", "input.nuevaCantidadProducto", function () {
     
     // var precioFinal = cantidad * nuevoPrecio;
     // precio.val(precioFinal);
-    $(".nuevoProducto .row").each(function () {
-        var idCantidadProducto = $(this)
-            .find(".nuevaCantidadProducto")
-            .attr("id");
-        var precio = $(this).find(".nuevoPrecioProducto");
-        var productoEncontrado = encontrarProducto(idCantidadProducto);
-
-        if (productoEncontrado) {
-            if(cantidad_acumulada >=  5 && nuevoStock < nuevoStockActual || 
-                cantidad_acumulada >= 7 && nuevoStock > nuevoStockActual){
-                nuevoPrecio = productoEncontrado.precio_compra;
-                console.log("entro");
-            }else{
-                console.log("no entro");
-                nuevoPrecio =  productoEncontrado.precio_venta;
-            }
-
-            var cantidad = parseFloat(
-                $(this).find(".nuevaCantidadProducto").val()
-            );
-            precio.val(nuevoPrecio * cantidad);
-        }
-    });
+    actualizarPrecios()
 
     $(this).attr("nuevoStock", nuevoStock);
 
