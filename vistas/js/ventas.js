8daa3e044f17aa97f72eb0a791a288ecb6096818
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2024-01-07 20:38:11 -0500

    test

--- vistas/js/ventas.js
+++ vistas/js/ventas.js
@@ -472,9 +472,11 @@ $(".formularioVenta").on("change", "input.nuevaCantidadProducto", function () {
     var nuevoStockActual = parseFloat($(this).attr("nuevoStock"));
     var nuevoStock = stock - cantidad;
 
-    if (cantidad_acumulada >= 5 && nuevoStock < nuevoStockActual || 
+    if (cantidad_acumulada >= 5 
+        // && nuevoStock < nuevoStockActual || 
         // cantidad_acumulada >= 5 && nuevoStock > nuevoStockActual || 
-        cantidad_acumulada >= 5 && productos_acumulado > 1 && nuevoStock < nuevoStockActual){
+        // cantidad_acumulada >= 5 && productos_acumulado > 1 && nuevoStock < nuevoStockActual
+        ){
         nuevoPrecio = elementoEncontrado.precio_compra;
     } else {
         nuevoPrecio = precioReal;
