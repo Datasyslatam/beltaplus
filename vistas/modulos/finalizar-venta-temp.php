561535030a402f70e38aa3c38df17f57cb7d20d9
Datasyslatam <datasys.latam@gmail.com>
2024-02-15 14:42:59 -0500

    Update fecha venta

--- vistas/modulos/finalizar-venta-temp.php
+++ vistas/modulos/finalizar-venta-temp.php
@@ -229,7 +229,7 @@ if ($_SESSION["perfil"] == "Especial") {
                       let codigoTransaccion = <?php echo json_encode($venta["metodo_pago"]); ?>;
                       let fecha_venta1 = <?php echo json_encode($venta["fecha_venta"]); ?>;
                       
-                      $('#nuevaFechaVenta').val($fecha_venta);
+                      $('#nuevaFechaVenta').val(fecha_venta1);
 
                       if (codigoTransaccion != 'Efectivo') {
                         let partesCodigoTransaccion = codigoTransaccion.split('-');
