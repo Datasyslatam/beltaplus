fb5baace112fff48679bee44145f7f8355b98f28
Alejandro.espinoza <alejandro.espinoza@grupo1522.com>
2024-03-18 12:57:55 -0500

    Refactor code for data import in cargue-masivo.php

--- vistas/modulos/cargue-masivo.php
+++ vistas/modulos/cargue-masivo.php
@@ -12,15 +12,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['dataCliente'])) {
         if ($i != 0) {
 
             $datos = explode(";", $linea);
-
-            $codigo = !empty($datos[1]) ? "'" . $datos[1] . "'" : "''";
-            $color = !empty($datos[2]) ? $datos[2] : '';
-            $categoria = !empty($datos[3]) ? $datos[3] : '';
-            $talla = !empty($datos[5]) ? $datos[5] : '';
-            $descripcion = !empty($datos[6]) ? $datos[6] : '';
-            $stock = !empty($datos[7]) ? intval($datos[7]) : 0;
-            $precioCompra = !empty($datos[9]) ? floatval($datos[9]) : 0.0;
-            $precioVenta = !empty($datos[8]) ? floatval($datos[8]) : 0.0;
+            $codigo = !empty($datos[0]) ? "'" . $datos[0] . "'" : "''";
+            $stock = !empty($datos[5]) ? intval($datos[5]) : 0;
 
             $checkCodigoDuplicado = "SELECT codigo FROM productos WHERE codigo = :codigo";
             $stmtCheck = Conexion::conectar()->prepare($checkCodigoDuplicado);
@@ -32,13 +25,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['dataCliente'])) {
             if ($cantDuplicados != 0) {
                 $updateData = "UPDATE productos SET
                             stock = :stock,
-                            precio_compra = :precioCompra,
-                            precio_venta = :precioVenta
                             WHERE codigo = :codigo";
                 $stmtUpdate = Conexion::conectar()->prepare($updateData);
                 $stmtUpdate->bindParam(':stock', $stock);
-                $stmtUpdate->bindParam(':precioCompra', $precioCompra);
-                $stmtUpdate->bindParam(':precioVenta', $precioVenta);
                 $stmtUpdate->bindParam(':codigo', $codigo);
                 $stmtUpdate->execute();
             }
