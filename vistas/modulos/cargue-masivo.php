c9943fa496c6e1c9073dd72453449b2ba2449658
Datasyslatam <datasys.latam@gmail.com>
2024-03-19 21:25:30 -0500

    Validac Cargue
    Cargue reg validacion

--- vistas/modulos/cargue-masivo.php
+++ vistas/modulos/cargue-masivo.php
@@ -7,17 +7,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['dataCliente'])) {
     $lineas     = file($archivotmp);
     $i = 0;
 
-    foreach ($lineas as $linea) {
+    foreach ($lineas as $linea) {/* 
         $cantidad_registros = count($lineas);
-        $cantidad_regist_agregados = ($cantidad_registros - 1);
+        $cantidad_regist_agregados = ($cantidad_registros - 1); */
 
         if ($i != 0) {
 
             $datos = explode(";", $linea);
-            $codigo = !empty($datos[0]) ? $datos[0] : '';
-            $stock = !empty($datos[5]) ? intval($datos[5]) : 0; 
-            /* $codigo = !empty($datos[0]) ? "'" . $datos[0] . "'" : "''";
-            $stock = !empty($datos[5]) ? intval($datos[5]) : 0; */
+            /* $codigo = !empty($datos[0]) ? $datos[0] : '';
+            $stock = !empty($datos[5]) ? intval($datos[5]) : 0;  */
+            $codigo = !empty($datos[0]) ? "'" . $datos[0] . "'" : "''";
+            $stock = !empty($datos[5]) ? intval($datos[5]) : 0;
 
             $checkCodigoDuplicado = "SELECT codigo FROM productos WHERE codigo = :codigo";
             $stmtCheck = Conexion::conectar()->prepare($checkCodigoDuplicado);
